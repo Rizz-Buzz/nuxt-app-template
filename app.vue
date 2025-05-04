5899e5b636bddf1924181e7fdb03fd37f86a1646
Hillary <tenskap@gmail.com>
2025-05-04 20:50:46 +0300

    add pages router and index page

--- app.vue
+++ app.vue
@@ -1,6 +1,46 @@
+<script setup lang="ts">
+
+useSeoMeta({
+  title: 'Nuxt App Template',
+  description: 'The Nuxt App Template'
+  // ogTitle: '[og:title]',
+  // ogDescription: '[og:description]',
+  // ogImage: '[og:image]',
+  // ogUrl: '[og:url]',
+  // twitterTitle: '[twitter:title]',
+  // twitterDescription: '[twitter:description]',
+  // twitterImage: '[twitter:image]',
+  // twitterCard: 'summary'
+})
+
+useHead({
+  htmlAttrs: {
+    lang: 'en'
+  },
+  link: [
+    {
+      rel: 'icon',
+      type: 'image/png',
+      href: '/favicon.png'
+    }
+  ]
+})
+</script>
+
 <template>
   <div>
-    <NuxtRouteAnnouncer />
-    <NuxtWelcome />
+    <NuxtPage />
   </div>
 </template>
+
+<style lang="scss">
+html {
+  @apply bg-gray-100;
+  font-family: 'Inter', sans-serif;
+}
+html,
+body,
+#__nuxt {
+  @apply h-full overflow-hidden;
+}
+</style>
---
+++ pages/index.vue
@@ -0,0 +1,9 @@
+<script setup lang="ts">
+
+</script>
+
+<template>
+  <div class="h-full">
+    <div class="pt-16 text-red-500 h-full">Home Page</div>
+  </div>
+</template>
