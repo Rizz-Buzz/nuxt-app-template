d8bb59f428b8898b7a01cf7a489be5a9f085c880
Hillary <tenskap@gmail.com>
2025-05-04 20:46:58 +0300

    Add tailwindcss

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,13 +1,17 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
+
   $development: {
     devServer: {
       port: process.env['PORT'] ? parseInt(process.env['PORT']) : undefined
     },
     devtools: { enabled: true }
   },
+
   runtimeConfig: {
     apiURL: '' // defined by NUXT_API_URL environment variable
   },
+
+  modules: ['@nuxtjs/tailwindcss'],
 })
