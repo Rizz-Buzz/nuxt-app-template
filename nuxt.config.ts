dd7d54a7a573958122c7c8a2f54cfcbb099856bc
Hillary <tenskap@gmail.com>
2025-05-04 20:12:09 +0300

    Add .env.example file and configure dev server port in nuxt.config.ts

--- nuxt.config.ts
+++ nuxt.config.ts
@@ -1,5 +1,10 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   compatibilityDate: '2024-11-01',
-  devtools: { enabled: true }
+  $development: {
+    devServer: {
+      port: process.env['PORT'] ? parseInt(process.env['PORT']) : undefined
+    },
+    devtools: { enabled: true }
+  }
 })
